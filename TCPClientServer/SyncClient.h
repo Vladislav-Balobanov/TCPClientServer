78fc5bf100d187cf0b43de17826e314876e4b8c7
vlad-is-love <vlad.velikan@gmail.com>
2022-05-18 11:09:49 +0700

    Конструктор для клиента.

--- TCPClientServer/SyncClient.h
+++ TCPClientServer/SyncClient.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <iostream>
 #include <boost/asio.hpp>
+#include <boost/thread.hpp>
 
 class SyncClient
 {
@@ -10,5 +12,11 @@ private:
 	boost::asio::io_service m_service;
 	boost::asio::ip::tcp::socket m_socket;
 	boost::asio::ip::tcp::endpoint m_endpoint;
+	boost::asio::io_context m_context;
+	std::thread m_thread;
 };
 
+SyncClient::SyncClient() :	m_endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 8001),
+							m_socket(m_service),
+							m_thread([&]() {m_socket.connect(m_endpoint); })
+{ }
