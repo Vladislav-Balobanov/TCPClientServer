d88082acb311eacced13cb192af784e42139c8da
vlad-is-love <vlad.velikan@gmail.com>
2022-05-19 17:12:44 +0700

    Создадим класс для клиента и объявим некоторые поля и методы.

--- TCPClientServer/Client.h
+++ TCPClientServer/Client.h
@@ -4,3 +4,16 @@
 
 using namespace boost::asio::ip;
 
+class Client
+{
+public:
+	Client() :	m_socket(m_ioService),
+				m_endpoint(address::from_string("127.0.0.1"), 80) 
+	{}
+	void syncConnect();
+	void disconnect();
+private:
+	boost::asio::io_service m_ioService;
+	tcp::socket m_socket;
+	tcp::endpoint m_endpoint;
+};
