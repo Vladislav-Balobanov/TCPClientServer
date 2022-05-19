c0ecb649827dfa0a6184dafc28e3f4b99ae551d4
vlad-is-love <vlad.velikan@gmail.com>
2022-05-19 18:22:26 +0700

    Создадим класс для сервера и объявим поля и некоторые методы.

--- TCPClientServer/Server.h
+++ TCPClientServer/Server.h
@@ -5,3 +5,22 @@
 
 using namespace boost::asio::ip;
 
+class Server
+{
+public:
+	Server() :	m_socket(m_ioService),
+				m_endpoint(address::from_string("127.0.0.1"), 80),
+				m_acceptor(m_ioService, m_endpoint)
+	{
+		m_ioService.run();
+	}
+	~Server()
+	{
+		m_ioService.stop();
+	}
+private:
+	boost::asio::io_service m_ioService;
+	tcp::socket m_socket;
+	tcp::endpoint m_endpoint;
+	tcp::acceptor m_acceptor;
+};
