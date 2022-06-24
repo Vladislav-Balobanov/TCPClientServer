1e49fbe429c0c1da7e1190fc3f9f82e1b9245c6d
vlad-is-love <vlad.velikan@gmail.com>
2022-06-24 11:25:04 +0700

    Добавим комментарии

--- TCPClientServer/main.cpp
+++ TCPClientServer/main.cpp
@@ -12,26 +12,35 @@ int main()
 	char command;
 	while(true)
 	{
+		// Блок работы с пользователем:
 		std::cout << "Hello, do you want to start messageing with server? insert y/n: ";
 		std::cin >> command;
 		if (command == 'N' || command == 'n')
 			break;
 
+		// Блок инициализации:
 		boost::asio::io_service ioService;
 		tcp::endpoint endpoint(address::from_string("127.0.0.1"), 80);
 		boost::system::error_code error;
 		boost::thread threadServer;
 		boost::thread threadClient;
 
+		// Блок подключения к клиента и сервера:
 		Server server(ioService, endpoint);
 		Client client(ioService, endpoint);
-
+		
+		// Запуск сервера:
 		threadServer = boost::thread([&]() {server.start(); });
+		// Синхронное подключение клиента:
 		client.syncConnect(error);
+		// Синкхронная запись строки:
 		threadClient = boost::thread([&]() {client.sendString("something!"); });
 		threadClient.join();
+		// Синхронное чтение строки:
 		threadServer = boost::thread([&]() {std::cout << "Getting steing is: \"" << server.getString() << "\"" << std::endl; });
 		threadServer.join();
+
+		// Работа с пользователем:
 		std::cout << "Hello, do you want to stop messageing with server? insert y/n: ";
 		std::cin >> command;
 		if (command == 'Y' || command == 'y')
